d6db2cf14a153a646c169c4c229f4f7489f5d54b
zakaria Echifaouy <108145379+zakaria0101echifaouy@users.noreply.github.com>
2023-10-19 15:07:42 +0100

    Create solution.py

---
+++ Backspace String Compare/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack_s = []
+        stack_t = []
+
+        for c in s:
+            if c == '#':
+                if stack_s:
+                    stack_s.pop()
+            else:
+                stack_s.append(c)
+        
+        for c in t:
+            if c == '#':
+                if stack_t:
+                    stack_t.pop()
+            else:
+                stack_t.append(c)
+        
+        return stack_s == stack_t
